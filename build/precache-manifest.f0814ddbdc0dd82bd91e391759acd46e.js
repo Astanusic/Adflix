ae38751169cf9478e50633934cb0ad9550014cc6
Astanusic <adrienpro.stanusic@gmail.com>
2020-04-24 21:22:37 +0200

    almost reponsive pages, still some styles to adjust

---
+++ build/precache-manifest.f0814ddbdc0dd82bd91e391759acd46e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22b1eec4eccc528d44519f6ccbdcd63c",
+    "url": "/lab/adflix/index.html"
+  },
+  {
+    "revision": "c91d64eb2360703cd2bc",
+    "url": "/lab/adflix/static/css/main.a36607f7.chunk.css"
+  },
+  {
+    "revision": "ee30656d3d748914b6f9",
+    "url": "/lab/adflix/static/js/2.6c8ed885.chunk.js"
+  },
+  {
+    "revision": "d891fcc30dfc707e593ab65e09f6fef9",
+    "url": "/lab/adflix/static/js/2.6c8ed885.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c91d64eb2360703cd2bc",
+    "url": "/lab/adflix/static/js/main.2193dd03.chunk.js"
+  },
+  {
+    "revision": "a8ea5d0b0705b1cf6627",
+    "url": "/lab/adflix/static/js/runtime-main.d30889e9.js"
+  },
+  {
+    "revision": "aa811e9dc7afca8a9b193629fb9fc41d",
+    "url": "/lab/adflix/static/media/28 Days Later.aa811e9d.woff"
+  },
+  {
+    "revision": "7ed8a39327099423f68b56e20df19d4d",
+    "url": "/lab/adflix/static/media/WickedScaryMovie.7ed8a393.woff"
+  }
+]);
--- src/components/HeaderImg.js
+++ src/components/HeaderImg.js
@@ -1,23 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import '../css/HeaderImg.css';
+import "../css/HeaderImg.css";
 
 class HeaderImg extends Component {
-    render() {
-        return (
-            <div 
-            className="headerImg"
-            style={{
-                background: `url(${this.props.imgScr}) no-repeat`
-                }}
-            >
-                <div className="headerImg--overlay">
-                    <h3 className="headerImg--overlay__title">{this.props.title}</h3>
-                    <p className="headerImg--overlay__desc">{this.props.overview}</p>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="headerImg">
+        <img src={this.props.imgScr} alt="" />
+        <div className="headerImg--overlay">
+          <h3 className="headerImg--overlay__title">{this.props.title}</h3>
+          <p className="headerImg--overlay__desc">{this.props.overview}</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export { HeaderImg };
